1f9fa9b17d78ca3e493992a10b61d8a8b8ef7a2f
Rakesh Sirikonda <sirikondarakesh001@gmail.com>
2020-06-20 02:42:01 +0530

    place order page done and sending data to firestore

--- truck/lib/main.dart
+++ truck/lib/main.dart
@@ -3,50 +3,54 @@ import 'package:flutter/material.dart';
 import 'package:truck/screens/homeScreen.dart';
 import 'package:truck/services/auth_services.dart';
 import './screens/loginScreen.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget with WidgetsBindingObserver {
   // This widget is the root of your application.
-  Widget screen(){
-      return StreamBuilder(
-        stream: FirebaseAuth.instance.onAuthStateChanged,
-        builder: (BuildContext context, snapshot) {
-          
-          
-            
-          //    if (snapshot.hasData) {
-          //   return HomeScreen();
-          // } else {
-          //   return LoginScreen();
-          // }});
-          // }
-          if (snapshot.hasData) {
-        if (snapshot.data.providerData.length == 1) { // logged in using email and password
-          return snapshot.data.isEmailVerified
-              ? HomeScreen()
-              : LoginScreen();
-        } else { // logged in using other providers
-          return HomeScreen();
+  Widget screen() {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.onAuthStateChanged,
+      builder: (BuildContext context, snapshot) {
+        if (snapshot.hasData) {
+          if (snapshot.data.providerData.length == 1) {
+            // logged in using email and password
+            return snapshot.data.isEmailVerified ? HomeScreen() : LoginScreen();
+          } else {
+            // logged in using other providers
+            return HomeScreen();
+          }
+        } else {
+          return LoginScreen();
         }
-      } else {
-        return LoginScreen();
-      }
-        },);
+      },
+    );
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Ship Tracking',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
+        accentColor: Colors.blue,
+        accentColorBrightness: Brightness.dark,
+        buttonTheme: ButtonTheme.of(context).copyWith(
+          buttonColor: Colors.orange,
+          textTheme: ButtonTextTheme.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: screen(),
-       routes: {
-                HomeScreen.routeName: (ctx) => HomeScreen(),},
+      routes: {
+        HomeScreen.routeName: (ctx) => HomeScreen(),
+      },
     );
   }
 }
--- truck/lib/screens/homeScreen.dart
+++ truck/lib/screens/homeScreen.dart
@@ -1,20 +1,76 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:truck/services/auth_services.dart';
 
 class HomeScreen extends StatefulWidget {
-   static const routeName = '/homeScreen';
+  static const routeName = '/homeScreen';
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-    _signOut() async {
-      _firebaseAuth == null? print(2):print(_firebaseAuth.currentUser());
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final Firestore _auth = Firestore.instance;
+  final _formKey = GlobalKey<FormState>();
+  var _vehicleNo = '';
+  var _truckNo = '';
+  var _tyreCheckUp = false;
+  var _tyres = 0;
+  String uid;
+  List<String> _tyresDD = ['Select tyres', '10', '12', '18'];
+  String _selectedTyres;
+  // String dropdownValue = 'Select Tyres';
+
+  @override
+  void initState() {
+    this.uid = '';
+    FirebaseAuth.instance.currentUser().then((value) {
+      setState(() {
+        this.uid = value.uid;
+        print("UID: " + uid);
+      });
+    }).catchError((e) {
+      print(e);
+    });
+    super.initState();
+  }
+
+  Future<void> _trySubmit() async {
+    FocusScope.of(context).unfocus();
+    final _isValid = _formKey.currentState.validate();
+    var _date = DateTime.now();
+
+    if (_isValid) {
+      _formKey.currentState.save();
+      print(_vehicleNo.trim());
+      print(_truckNo.trim());
+      print(_selectedTyres);
+
+      await Firestore.instance.collection('/users/${uid}/orders').add({
+        'Ship Number': _vehicleNo,
+        'Truck Number': _truckNo,
+        'Tyres': _selectedTyres,
+        'date': _date
+      });
+      // print("BuildContext:".context);
+      // Scaffold.of(context).showSnackBar(SnackBar(
+      //   content: Text("vehicle:" +
+      //       _vehicleNo +
+      //       " truck:" +
+      //       _truckNo +
+      //       " tyres:" +
+      //       _selectedTyres),
+      //       duration: Duration(seconds: 5),
+      // ));
+    }
+  }
+
+  _signOut() async {
+    _firebaseAuth == null ? print(2) : print(_firebaseAuth.currentUser());
     await _firebaseAuth.signOut();
-    
-   }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,14 +97,122 @@ class _HomeScreenState extends State<HomeScreen> {
               ],
               onChanged: (itemIdentifier) {
                 if (itemIdentifier == 'logout') {
-                                // AuthService().signOut();
-                                _signOut();
-                                                                  }
-                                }),
-                          ],
-                        ),
-                        body: Container(),
-                      );
-                    }
-                  
+                  // AuthService().signOut();
+                  _signOut();
+                }
+              }),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: TextFormField(
+                key: ValueKey('VehicleNo'),
+                decoration: InputDecoration(
+                  labelText: "Vehicle No",
+                  fillColor: Colors.white,
+                  border: new OutlineInputBorder(
+                    borderRadius: new BorderRadius.circular(25.0),
+                    borderSide: new BorderSide(),
+                  ),
+                ),
+                onSaved: (value) {
+                  _vehicleNo = value;
+                },
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Vehicle number cann\'t be empty";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: TextFormField(
+                key: ValueKey('TruckNo'),
+                decoration: InputDecoration(
+                  labelText: "Truck No",
+                  fillColor: Colors.white,
+                  border: new OutlineInputBorder(
+                    borderRadius: new BorderRadius.circular(25.0),
+                    borderSide: new BorderSide(),
+                  ),
+                ),
+                onSaved: (value) {
+                  _truckNo = value;
+                },
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Truck Number cann\'t be empty";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Center(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 10.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                  border: Border.all(
+                      color: Theme.of(context).primaryColor,
+                      style: BorderStyle.solid,
+                      width: 0.80),
+                ),
+                child: DropdownButton(
+                  hint: Text('Select tyres'),
+                  value: _selectedTyres,
+                  onChanged: (newValue) {
+                    setState(() {
+                      _selectedTyres = newValue;
+                    });
+                  },
+                  items: _tyresDD.map((tyre) {
+                    return DropdownMenuItem(
+                      child: new Text(tyre),
+                      value: tyre,
+                    );
+                  }).toList(),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Container(
+              padding: EdgeInsets.all(16),
+              child: Center(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.85,
+                  child: RaisedButton(
+                    color: Theme.of(context).primaryColor,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Text(
+                      "SUBMIT",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: () {
+                      _trySubmit();
+                    },
+                    padding: EdgeInsets.all(16.0),
+                  ),
+                ),
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
 }
