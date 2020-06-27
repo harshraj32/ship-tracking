e2b56248a0cd9d64ecd86a563df06153db2183af
meenansai <v.saimeenan@gmail.com>
2020-06-27 17:06:58 +0530

    polylines added

--- truck/lib/screens/checkStatusScreen.dart
+++ truck/lib/screens/checkStatusScreen.dart
@@ -1,10 +1,10 @@
 import 'dart:async';
-import 'dart:typed_data';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
 class CheckStatus extends StatefulWidget {
   static const route = '/checkstatus';
@@ -15,14 +15,58 @@ class CheckStatus extends StatefulWidget {
 class _CheckStatusState extends State<CheckStatus> {
   String uid;
   Completer<GoogleMapController> _controller = Completer();
-
+  List<LatLng> latlng1 = [];
+  double _originLatitude = 17.4401, _originLongitude = 78.3489;
+  double _destLatitude = 18.8762165, _destLongitude = 79.998875;
   BitmapDescriptor pinLocationIcon;
   BitmapDescriptor pinLocationIcon2;
+  Set<Marker> _markers = {};
+  Map<PolylineId, Polyline> polylines = {};
+  List<LatLng> polylineCoordinates = [];
+  PolylinePoints polylinePoints = PolylinePoints();
+  String googleAPIKey = "AIzaSyAx1w469aSv5bKV8u0YJg3M_Lt-grDkEgo";
 
-  // static const LatLng _center = const LatLng(45.521563, -122.677433);
+  @override
+  void didChangeDependencies() {
+    _getPolyline();
+    
+    super.didChangeDependencies();
+  }
 
-  void _onMapCreated(GoogleMapController controller) {
+  void _onMapCreated(GoogleMapController controller)async  {
     _controller.complete(controller);
+    
+  }
+
+  _addPolyLine() {
+    print(' Inside addployline function ');
+    PolylineId id = PolylineId("poly");
+    Polyline polyline = Polyline(
+      width: 5,
+      polylineId: id,
+      color: Colors.blue,
+      points: latlng1,
+      visible: true,
+    );
+    polylines[id] = polyline;
+    setState(() {});
+  }
+
+  _getPolyline() async {
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      googleAPIKey,
+      PointLatLng(_originLatitude, _originLongitude),
+      PointLatLng(_destLatitude, _destLongitude),
+      // travelMode: TravelMode.driving,
+      // wayPoints: [PolylineWayPoint(location: "Sabo, Yaba Lagos Nigeria")]
+    );
+    if (result.points.isNotEmpty) {
+      result.points.forEach((PointLatLng point) {
+        latlng1.add(LatLng(point.latitude, point.longitude));
+      });
+    }
+    print("before calling addpolyline");
+    _addPolyLine();
   }
 
   @override
@@ -49,6 +93,22 @@ class _CheckStatusState extends State<CheckStatus> {
 
   @override
   Widget build(BuildContext context) {
+    print(latlng1);
+    print(latlng1.length);
+    if (latlng1.length != 0) {
+      setState(() {
+        _originLatitude = num.parse(latlng1[0].latitude.toStringAsFixed(5));
+        _originLongitude = num.parse(latlng1[0].longitude.toStringAsFixed(5));
+        _destLatitude =
+            num.parse(latlng1[latlng1.length - 1].latitude.toStringAsFixed(5));
+        _destLongitude =
+            num.parse(latlng1[latlng1.length - 1].longitude.toStringAsFixed(5));
+      });
+    }
+    print(_originLatitude.toString()+" org lat");
+    print(_originLongitude.toString()+" org lon");
+    print(_destLatitude.toString()+" dest lat");
+    print(_destLongitude.toString()+" dest lon");
     Map<String, Object> docId = ModalRoute.of(context).settings.arguments;
     if (docId != null) {
       print("docID:" + docId['docId']);
@@ -63,7 +123,7 @@ class _CheckStatusState extends State<CheckStatus> {
               .document(uid)
               .collection('orders')
               .document(docId['docId'])
-              .collection('coordinates')
+              .collection('coordinates').orderBy('timestamp',descending: true)
               .snapshots(),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
@@ -72,45 +132,53 @@ class _CheckStatusState extends State<CheckStatus> {
               );
             }
             var doc = snapshot.data.documents;
+            print('Tracking docs: ' + doc.toString());
             var len = doc.length;
             List<LatLng> latlng = [];
+            
             for (int i = 0; i < doc.length; i++) {
               latlng.add(LatLng(
                   double.parse(doc[i]['lat']), double.parse(doc[i]['lng'])));
             }
-            return GoogleMap(
-              markers: Set<Marker>.of(latlng.map((e) {
-                
-                if(latlng[0].latitude==e.latitude && latlng[0].longitude==e.longitude){
-                  return Marker(
-                  markerId: MarkerId('rakesh'),
-                  position: e,
-                  
-                );
-                }
-                else if(latlng[len-1].latitude==e.latitude&& latlng[len-1].longitude==e.longitude ){
-                  return Marker(
-                  markerId: MarkerId('rakesh'),
-                  position: e,
-                  icon: pinLocationIcon
-                );
-                }
-                else{
-                  return Marker(
-                  markerId: MarkerId('rakesh'),
-                  position: e,
-                );
-                }
-               
-                
-              }).toList()),
-              onMapCreated: _onMapCreated,
-              initialCameraPosition: CameraPosition(
-                target: LatLng(double.parse(doc[len - 1]['lat']),
-                    double.parse(doc[len - 1]['lng'])),
-                zoom: 11.0,
-              ),
-            );
+            latlng1=latlng;
+            return len == 0
+                ? Center(
+                    child: Text('ship is not assigned yet'),
+                  )
+                : GoogleMap(
+                    polylines: Set<Polyline>.of(polylines.values),
+                    myLocationEnabled: true,
+                    compassEnabled: true,
+                    tiltGesturesEnabled: false,
+                    mapType: MapType.normal,
+                    markers: Set<Marker>.of(latlng.map((e) {
+                      if (latlng[0].latitude == e.latitude &&
+                          latlng[0].longitude == e.longitude) {
+                        return Marker(
+                          markerId: MarkerId('rakesh'),
+                          position: e,
+                        );
+                      } else if (latlng[len - 1].latitude == e.latitude &&
+                          latlng[len - 1].longitude == e.longitude) {
+                        return Marker(
+                            markerId: MarkerId('rakesh'),
+                            position: e,
+                            icon: pinLocationIcon);
+                      } else {
+                        return Marker(
+                          markerId: MarkerId('rakesh'),
+                          position: e,
+                          icon: BitmapDescriptor.defaultMarkerWithHue(200)
+                        );
+                      }
+                    }).toList()),
+                    onMapCreated: _onMapCreated,
+                    initialCameraPosition: CameraPosition(
+                      target: LatLng(double.parse(doc[len - 1]['lat']),
+                          double.parse(doc[len - 1]['lng'])),
+                      zoom: 5.0,
+                    ),
+                  );
           }),
     );
   }
